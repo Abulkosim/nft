057462e780c7ad28b181e1b4142773189ac8e41b
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-10-27 01:23:24 +0500

    intro section in progress

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-dark-gray text-white">
     <Header />
+    <Intro />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Intro from "./components/Intro.vue";
 </script>
---
+++ src/components/Intro.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container px-4 mx-auto py-10">
+    <div class="max-w-[510px] shadow-lg">
+      <img src="../assets/space.png" alt="Space" class="w-full">
+      <div class="flex flex-col gap-2 bg-gray p-4 rounded-b-[20px]">
+        <span class="text-xl">Space Walking</span>
+        <div class="flex items-center gap-2">
+          <img src="../assets/profile.png" alt="Profile">
+          <span>Animakid</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PhList } from "@phosphor-icons/vue";
+
+</script>
