06bab024755408ef01e9160015c00781d249d6df
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-10-28 14:35:07 +0500

    works section

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <Collection v-motion-slide-visible-once-bottom />
     <Creators v-motion-slide-visible-once-bottom />
     <Categories v-motion-slide-visible-once-bottom />
+    <Works v-motion-slide-visible-once-bottom />
     <Subscription v-motion-slide-visible-once-bottom />
   </div>
 </template>
@@ -15,5 +16,6 @@ import Intro from "./components/Intro.vue";
 import Collection from "./components/Collection.vue";
 import Creators from "./components/Creators.vue";
 import Categories from "./components/Categories.vue";
+import Works from "./components/Works.vue";
 import Subscription from "./components/Subscription.vue";
 </script>
--- src/components/Subscription.vue
+++ src/components/Subscription.vue
@@ -3,14 +3,14 @@
     <div class="container mx-auto px-7 py-10">
 
       <div
-        class="md:p-10 lg:p-14 flex flex-col md:flex-row items-center rounded-[20px] sm:bg-gray gap-7 lg:gap-20 sm:shadow-xl"
+        class="md:p-10 lg:p-14 flex flex-col md:flex-row items-center rounded-[20px] md:bg-gray gap-7 lg:gap-20 md:shadow-xl"
         v-motion-slide-visible-once-right>
 
         <div class="basis-1/2 rounded-[20px] shadow-xl">
           <img src="../assets/cosmos.png" alt="Cosmos" class="w-full rounded-[20px] opacity-95 hover:opacity-100">
         </div>
         <div class="basis-1/2 flex flex-col gap-3">
-          <h3 class="text-2xl md:text-3xl font-semibold lg:text-4xl">Join Our Weekly Digest</h3>
+          <h3 class="text-2xl sm:text-3xl font-semibold lg:text-4xl">Join Our Weekly Digest</h3>
           <p class="text-lg md:text-xl">Get Exclusive Promotions & Updates Straight to Your Inbox.</p>
 
           <a href="#"
