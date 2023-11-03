fc5291840b38d9d654bf7f139caff947417c652e
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-11-03 12:27:59 +0500

    routing problems

--- src/router/router.js
+++ src/router/router.js
@@ -2,19 +2,20 @@ import { createWebHistory, createRouter } from "vue-router";
 import Main from "../views/Main.vue";
 import Signup from "../views/Signup.vue";
 
-const history = createWebHistory();
+const routes = [
+  {
+    path: "/nft",
+    component: Main,
+  },
+  {
+    path: "/nft/signup",
+    component: Signup,
+  },
+];
+
 const router = createRouter({
-  history,
-  routes: [
-    {
-      path: "/nft/",
-      component: Main,
-    },
-    {
-      path: "/nft/signup",
-      component: Signup,
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
